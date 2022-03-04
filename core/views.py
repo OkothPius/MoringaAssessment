b7d1f998190be08f7041226bb890cdcab72921d4
Okoth Pius <orukopius8@gmail.com>
2022-03-04 16:48:54 +0300

    feat: update core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Game
 
-# Create your views here.
+
+class GameListView(ListView):
+    """
+    Class Based View listing the user information and game details.
+    """
+    model = Game
+    template_name = 'core/index.html'
+    context_object_name = 'games'
+    paginate_by = 2
+
+
+class GameCreateView(CreateView):
+    """
+    Class Based View detailing how to create user information.
+    """
+    model = Game
+    fields = ['name', 'viewer_hour', 'hours_streamed', 'acv_num', 'creators', 'streams_num']
