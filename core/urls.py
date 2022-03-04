8d5247b1a8d674fc5f2deeec8680917e1a3541d5
Okoth Pius <orukopius8@gmail.com>
2022-03-04 19:00:54 +0300

    feat: update urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import GameListView, GameCreateView
+from .views import GameListView, GameCreateView, GameUpdateView, GameDeleteView
 
 urlpatterns = [
     path('', GameListView.as_view(), name='home'),
     path('new/', GameCreateView.as_view(), name='game-create'),
+    path('game/<int:pk>/update/', GameUpdateView.as_view(), name='game-update'),
+    path('game/<int:pk>/delete/', GameDeleteView.as_view(), name='game-delete'),
+
 ]
